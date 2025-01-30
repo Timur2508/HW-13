6fc554b91cc0b41aaad5f993e7d5dec9a6b8c238
Timur2508 <dispetcher89@list.ru>
2025-01-30 16:34:05 +0500

    дз на проверку

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -5,18 +5,15 @@ import org.skypro.skyshop.basket.ProductBasket;
 
 public class App {
     public static void main(String[] args) {
-        // Создание экземпляра корзины
         ProductBasket basket = new ProductBasket();
 
-        // Создание нескольких продуктов
         Product apple = new Product("Яблоко", 50);
         Product bread = new Product("Хлеб", 30);
         Product milk = new Product("Молоко", 40);
         Product meat = new Product("Мясо", 150);
         Product fish = new Product("Рыба", 200);
-        Product cheese = new Product("Сыр", 120); // Продукт для проверки переполнения корзины
+        Product cheese = new Product("Сыр", 120);
 
-        // 1. Добавление продукта в корзину
         System.out.println("Добавление продуктов в корзину:");
         basket.addProduct(apple);
         basket.addProduct(bread);
@@ -24,39 +21,30 @@ public class App {
         basket.addProduct(meat);
         basket.addProduct(fish);
 
-        // 2. Попытка добавить продукт в заполненную корзину
         System.out.println("Попытка добавить продукт в заполненную корзину:");
-        basket.addProduct(cheese); // Должно вывести сообщение "Невозможно добавить продукт"
+        basket.addProduct(cheese);
 
-        // 3. Печать содержимого корзины с несколькими товарами
         System.out.println("\nСодержимое корзины:");
-        basket.printBasket(); // Должно показать все добавленные продукты и итоговую стоимость
+        basket.printBasket();
 
-        // 4. Получение стоимости корзины с несколькими товарами
         int totalCost = basket.getTotalCost();
         System.out.println("\nОбщая стоимость корзины: " + totalCost);
 
-        // 5. Поиск товара, который есть в корзине
         boolean containsApple = basket.containsProduct("Яблоко");
         System.out.println("Корзина содержит яблоко: " + containsApple);
 
-        // 6. Поиск товара, которого нет в корзине
         boolean containsBanana = basket.containsProduct("Банан");
         System.out.println("Корзина содержит банан: " + containsBanana);
 
-        // 7. Очистка корзины
         basket.clearBasket();
         System.out.println("\nКорзина очищена.");
 
-        // 8. Печать содержимого пустой корзины
         System.out.println("\nСодержимое корзины:");
-        basket.printBasket(); // Должно вывести "В корзине пусто"
+        basket.printBasket();
 
-        // 9. Получение стоимости пустой корзины
         totalCost = basket.getTotalCost();
         System.out.println("Общая стоимость пустой корзины: " + totalCost);
 
-        // 10. Поиск товара по имени в пустой корзине
         containsApple = basket.containsProduct("Яблоко");
         System.out.println("Корзина содержит яблоко: " + containsApple);
     }
--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,21 +1,18 @@
 package org.skypro.skyshop.product;
 
 public class Product {
-    private final String name; // Название продукта
-    private final int price;    // Стоимость продукта
+    private final String name;
+    private final int price;
 
-    // Конструктор класса
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
-    // Метод для получения названия продукта
     public String getName() {
         return name;
     }
 
-    // Метод для получения стоимости продукта
     public int getPrice() {
         return price;
     }
